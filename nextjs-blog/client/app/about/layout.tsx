f94224656222a9600d2532850f54ffc422e9f28a
sk <jsckhan@outlook.com>
2023-03-13 20:17:49 -0400

    fix: nextjs-blog/ vite dep bug for postcss
    Vite reads the postcss config file that is in the client project
    structure. Having the sanity studio in the browser client directory
    then runs into an error with launching the sanity studio client. Moving
    the sanity-studio to it's own folder, and pushed the browser client
    into it's own project.

---
+++ nextjs-blog/client/app/about/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function AboutLayout({ children }: Props) {
+  return <>{children}</>;
+}
---
+++ nextjs-blog/client/app/about/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Page() {
+  return <div>Hi this is page</div>;
+}
