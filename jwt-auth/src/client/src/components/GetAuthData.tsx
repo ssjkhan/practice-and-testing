e5dc3a0a6b115e3ebb96eecb87e8b77b974ae46a
sk <jsckhan@outlook.com>
2023-04-04 23:33:27 -0400

    jwt-auth/ feat: data feteching in component

--- jwt-auth/src/client/src/components/GetAuthData.tsx
+++ jwt-auth/src/client/src/components/GetAuthData.tsx
@@ -1,3 +1,27 @@
-export default function GetAuthData() {
-  return <div className="form-el">Auth Data</div>;
+import DataService from "@services/getAuthData";
+import { useEffect, useState } from "react";
+
+export default function GetData() {
+  // const resp = await DataService();
+  const [state, setState] = useState({});
+  function handleClick() {
+    DataService().then((res) => {
+      setState(res);
+    }).catch((_) => console.log("oops getAuthData Failed"));
+  }
+
+  useEffect(() => {
+    async function getAuthData() {
+      const data = await DataService();
+      setState(data);
+    }
+  }, []);
+
+  // console.log(state);
+  return (
+    <div className="form-el">
+      <button onClick={handleClick}>Get Auth Data</button>
+      <div>{JSON.stringify(state)}</div>
+    </div>
+  );
 }
--- jwt-auth/src/client/src/components/GetData.tsx
+++ jwt-auth/src/client/src/components/GetData.tsx
@@ -1,3 +1,26 @@
+import DataService from "@services/getData";
+import { useEffect, useState } from "react";
+
 export default function GetData() {
-  return <div className="form-el">This is Get Data Func</div>;
+  const [state, setState] = useState({});
+  // const resp = await DataService();
+  async function handleClick() {
+    const data = await DataService();
+    setState(data);
+  }
+
+  // useEffect(() => {
+  //   async function getData() {
+  //     const data = await DataService();
+  //     setState(state);
+  //   }
+  // }, []);
+
+  // console.log(state);
+  return (
+    <div className="form-el">
+      <button onClick={handleClick}>Get Data</button>
+      <div>{JSON.stringify(state)}</div>
+    </div>
+  );
 }
